ea9ab8ddbf7811a1792907ef3fa2356657c7e71d
t1m3ctrl <t1m3ctrl@gmail.com>
2023-03-01 16:11:23 +0700

    Added func CircleExtractor

--- main.c
+++ main.c
@@ -8,55 +8,62 @@
 #define RESET "\033[0m"
 #define RED "\033[1;31m"
 
-void StringParser(char* str, long int len, int line);
-int BracketsErrorChecker(const char* str, int line, long int name_len);
-int NameErrorChecker(const char* str, int line);
-int UnexpectedTokenErrorChecker(const char* str, int line);
-int ContainerErrorChecker(char* str, int line);
+typedef struct {
+    float x;
+    float y;
+} Point;
+
+typedef struct {
+    Point center;
+    float radius;
+    float perimeter;
+    float area;
+    int* intersects;
+} Circle;
 
 // typedef struct {
-//     float x;
-//     float y;
-// } point;
-
-// typedef struct {
-//     int id;
-//     char figure_name[15];
-//     point center;
-//     float radius;
+//     Point apex[3];
 //     float perimeter;
 //     float area;
 //     int* intersects;
-// } circle;
+// } Triangle;
 
 // typedef struct {
-//     int id;
-//     char figure_name[15];
-//     point apex[3];
+//     Point* apex;
 //     float perimeter;
 //     float area;
 //     int* intersects;
-// } triangle;
+// } Polygon;
 
-// typedef struct {
-//     int id;
-//     char figure_name[15];
-//     point* apex;
-//     float perimeter;
-//     float area;
-//     int* intersects;
-// } polygon;
+int BasicErrorChecker(char* str, long int len, char* name, int line);
+int BracketsErrorChecker(char* str, int line, long int name_len);
+int NameErrorChecker(char* str, int line);
+int UnexpectedTokenErrorChecker(char* str, int line);
+Circle* CircleExtractor(char* str, long int len, int line);
+int CircleContentErrorChecker(char* str, long int len, int line);
+// Triangle* TriangleExtractor(char* str, long int len, int line);
+// Polygon* PolygonExtractor(char* str, long int len, int line);
+// int ContainerErrorChecker(char* str, int line);
+int isNumber(char* str, int line);
 
 int main()
 {
     FILE* geometry;
 
-    // triangle* triangles;
-    // circle* circles;
-    // polygon* polygons;
-    char str1[MAX_SIZE];
+    Circle circles[20]; // circles = (Circle*)malloc(line * sizeof(Circle));
+    Circle* tmp_circle;
 
-    int line;
+    // Triangle triangles[20];
+    // Triangle* tmp_triangle;
+
+    // Polygon polygons[20];
+    // Polygon* tmp_polygon;
+
+    char str1[MAX_SIZE];
+    char figure_name[][10] = {"circle", "triangle", "polygon"};
+
+    int line, error_num = 0, id_circle = 0;
+    // int id_triangle = 0, id_polygon = 0;
 
     geometry = fopen("geometry.txt", "r");
 
@@ -66,42 +73,74 @@ int main()
         line = 1;
         while (!feof(geometry)) {
             if (fgets(str1, MAX_SIZE, geometry) != NULL) {
-                printf("%s", str1);
-                StringParser(str1, strlen(str1) + 1, line); // +1 cause of '\0'
+                int len = strlen(str1);
+                char name[len];
+
+                for (int i = 0; i < len; i++) {
+                    if (str1[i] == '(') {
+                        name[i] = '\0';
+                        break;
+                    }
+                    name[i] = tolower(str1[i]);
+                }
+
+                error_num = BasicErrorChecker(str1, len, name, line);
+
+                if (error_num == 0) {
+                    if (strncmp(name, figure_name[0], strlen(figure_name[0]))
+                        == 0) {
+                        if (CircleContentErrorChecker(str1, len, line) == 0) {
+                            tmp_circle = CircleExtractor(str1, len, line);
+                            circles[id_circle] = *tmp_circle;
+                            id_circle++;
+                        }
+                    }
+
+                    // if (strncmp(name, figure_name[1], strlen(figure_name[1]))
+                    //     == 0) {
+                    //     tmp_triangle = TriangleExtractor(str1, len, line);
+                    //     triangles[id_triangle] = *tmp_triangle;
+                    //     id_triangle++;
+                    // }
+
+                    // if (strncmp(name, figure_name[2], strlen(figure_name[2]))
+                    //     == 0) {
+                    //     tmp_polygon = PolygonExtractor(str1, len, line);
+                    //     polygons[id_polygon] = *tmp_polygon;
+                    //     id_polygon++;
+                    // }
+                }
                 line++;
             }
         }
         fclose(geometry);
     }
+
+    for (int i = 0; i < id_circle; i++) {
+        printf("%d. circle(%.2lf %.2lf, %.2lf)\n",
+               i + 1,
+               circles[i].center.x,
+               circles[i].center.y,
+               circles[i].radius);
+    }
     return 0;
 }
 
-void StringParser(char* str, long int len, int line)
+int BasicErrorChecker(char* str, long int len, char* name, int line)
 {
-    int i, error_num = 0;
-    char name[len];
-
-    for (i = 0; i < len; i++) {
-        if (str[i] == '(') {
-            name[i] = '\0';
-            break;
-        }
-        name[i] = tolower(str[i]);
-    }
+    int error_num = 0;
 
     error_num = NameErrorChecker(name, line);
     if (error_num == 0) {
         error_num = BracketsErrorChecker(str, line, strlen(name) - 1);
         if (error_num == 0) {
             error_num = UnexpectedTokenErrorChecker(str, line);
-            // if (error_num == 0) {
-            //     error_num = ContainerErrorChecker(str, line);
-            // }
         }
     }
+    return error_num;
 }
 
-int BracketsErrorChecker(const char* str, int line, long int name_len)
+int BracketsErrorChecker(char* str, int line, long int name_len)
 {
     int column = 0;
     int error_column_open;
@@ -121,8 +160,8 @@ int BracketsErrorChecker(const char* str, int line, long int name_len)
                 if (--stack < 0) {
                     if (column_flag == 1) {
                         error_flag_brackets = 1;
-                        printf("%sError: expected '(' [line %d "
-                               "column %d]%s\n",
+                        printf("%sError: expected '(' [line %d column "
+                               "%d]%s\n",
                                RED,
                                line,
                                error_column_open + 1,
@@ -130,8 +169,8 @@ int BracketsErrorChecker(const char* str, int line, long int name_len)
                         break;
                     } else if (column_flag == 0) {
                         error_flag_brackets = 1;
-                        printf("%sError: expected '(' [line %d "
-                               "column %ld]%s\n",
+                        printf("%sError: expected '(' [line %d column "
+                               "%ld]%s\n",
                                RED,
                                line,
                                name_len,
@@ -155,7 +194,7 @@ int BracketsErrorChecker(const char* str, int line, long int name_len)
     return error_flag_brackets;
 }
 
-int NameErrorChecker(const char* str, int line)
+int NameErrorChecker(char* str, int line)
 {
     char figure_name[][10] = {"circle", "triangle", "polygon"};
     int i, error_flag_name = 2;
@@ -165,7 +204,8 @@ int NameErrorChecker(const char* str, int line)
         }
     }
     if (error_flag_name != 0)
-        printf("%sError: expected 'circle', 'triangle' or 'polygon' [line %d "
+        printf("%sError: expected 'circle', 'triangle' or 'polygon' [line "
+               "%d "
                "column 0]%s\n",
                RED,
                line,
@@ -173,7 +213,7 @@ int NameErrorChecker(const char* str, int line)
     return error_flag_name;
 }
 
-int UnexpectedTokenErrorChecker(const char* str, int line)
+int UnexpectedTokenErrorChecker(char* str, int line)
 {
     int error_flag_token = 0;
 
@@ -181,11 +221,6 @@ int UnexpectedTokenErrorChecker(const char* str, int line)
 
     bracket_closed_last = strrchr(str, ')');
 
-    // printf("bracket_closed_last - str = %ld, strlen(str) = %ld\n",
-    //        bracket_closed_last - str,
-    //        strlen(str));
-
-    // bracket_closed_last - str + 2 (+1 - \n)
     if (bracket_closed_last - str + 2 != strlen(str)) {
         error_flag_token = 3;
         printf("%sError: unexpected token [line %d "
@@ -199,90 +234,122 @@ int UnexpectedTokenErrorChecker(const char* str, int line)
     return error_flag_token;
 }
 
-// int ContainerErrorChecker(char* str, int line)
+Circle* CircleExtractor(char* str, long int len, int line)
+{
+    Circle* temporary = (Circle*)malloc(sizeof(Circle));
+    char* bracket_open_first = strchr(str, '(');
+    char* bracket_closed_last = strrchr(str, ')');
+    int i, j;
+    char container[len], *istr = NULL;
+    char sep[2] = ", ";
+
+    for (i = bracket_open_first - str + 1, j = 0; i < bracket_closed_last - str;
+         i++, j++) {
+        container[j] = str[i];
+        // printf("%c", container[j]);
+    }
+    container[j + 1] = '\0';
+
+    istr = strtok(container, sep);
+    temporary->center.x = atof(istr);
+    istr = strtok(NULL, sep);
+    temporary->center.y = atof(istr);
+    istr = strtok(NULL, sep);
+    temporary->radius = atof(istr);
+
+    return temporary;
+}
+
+int CircleContentErrorChecker(char* str, long int len, int line)
+{
+    char* bracket_open_first = strchr(str, '(');
+    char* bracket_closed_last = strrchr(str, ')');
+    int i, j, error_extract = 0;
+    char container[len], *istr = NULL;
+    char sep[2] = ", ";
+
+    for (i = bracket_open_first - str + 1, j = 0; i < bracket_closed_last - str;
+         i++, j++) {
+        container[j] = str[i];
+    }
+    container[j + 1] = '\0';
+
+    istr = strtok(container, sep);
+    error_extract = isNumber(istr, line);
+    if (error_extract == 0) {
+        istr = strtok(NULL, sep);
+        error_extract = isNumber(istr, line);
+        if (error_extract == 0) {
+            istr = strtok(NULL, sep);
+            error_extract = isNumber(istr, line);
+            if (error_extract == 0) {
+                istr = strtok(NULL, sep);
+                if (istr != NULL)
+                    printf("%sError: unexpected token in brackets [line "
+                           "%d]%s\n",
+                           RED,
+                           line,
+                           RESET);
+            }
+        }
+    }
+
+    return error_extract;
+}
+
+// Triangle* TriangleExtractor(char* str, long int len, int line)
 // {
-//     int i, j, k = 0, error_flag_container = 0, floating_point = 0,
-//     error_column,
-//               multipart = 0;
-
-//     char* container;
-//     char** multipart_content;
-//     char* istr;
-
-//     char sep[2] = ", ";
-
-//     char* bracket_open_last;
-//     char* bracket_closed_last;
-//     char* bracket_open_first;
-//     char* bracket_closed_first;
-
-//     bracket_open_first = strchr(str, '(');
-//     bracket_closed_last = strrchr(str, ')');
-
-//     container = (char*)malloc(
-//             (bracket_closed_last - str - (bracket_open_first - str + 1))
-//             * sizeof(char));
-
-//     for (i = bracket_open_first - str + 1, j = 0; i < bracket_closed_last -
-//     str;
-//          i++, j++) {
-//         container[j] = str[i];
-//         if (str[i] == '(')
-//             multipart++;
-//     }
-
-//     multipart_content = (char**)malloc(multipart * sizeof(char*));
-
-//     do {
-//         bracket_open_first = strchr(container, '(');
-//         bracket_closed_first = strchr(container, ')');
-//         multipart_content = (char*)malloc(
-//                 (bracket_closed_first - container
-//                  - (bracket_open_first - container + 1))
-//                 * sizeof(char));
-//         for (i = bracket_open_first - str + 1, j = 0;
-//              i < bracket_closed_first - str;
-//              i++, j++) {
-//             multipart_content[k][j] = str[i];
-//         }
-//         k++;
-//     } while (k < multipart);
-
-//     error_column = bracket_open_last - str + 1;
-//     istr = strtok(container, sep);
-//     error_column++;
-//     while (istr != NULL) {
-//         floating_point = 0;
-//         for (i = 0; i < strlen(istr); i++) {
-//             error_column++;
-//             if ((i == 0) && (istr[i] == '-'))
-//                 continue;
-//             if (((i >= 0) || (i <= strlen(istr) - 1)) && (istr[i] == '.')) {
-//                 floating_point++;
-//                 if (floating_point > 1) {
-//                     error_flag_container = 4;
-//                     printf("%sError: expected <double> [line %d "
-//                            "column %d]%s\n",
-//                            RED,
-//                            line,
-//                            error_column - 2,
-//                            RESET);
-//                     break;
-//                 }
-//             }
-//             if ((isdigit(istr[i]) == 0) && (istr[i] != '.')) {
-//                 error_flag_container = 4;
-//                 printf("%sError: expected <double> [line %d "
-//                        "column %d]%s\n",
-//                        RED,
-//                        line,
-//                        error_column - 2,
-//                        RESET);
-//                 break;
-//             }
-//         }
-//         istr = strtok(NULL, sep);
-//         error_column++;
-//     }
-//     return error_flag_container;
 // }
+
+// Polygon* PolygonExtractor(char* str, long int len, int line)
+// {
+// }
+
+int isNumber(char* str, int line)
+{
+    int floating_point = 0;
+    int i;
+    int minus = 0;
+    int error_flag_container = 0;
+    int len = strlen(str);
+
+    for (i = 0; i < len; i++) {
+        if ((i == 0) && (str[i] == '-')) {
+            minus = 1;
+            continue;
+        } else if ((isdigit(str[minus]) == 0) || (isdigit(str[len - 1]) == 0)) {
+            error_flag_container = 4;
+            printf("%sError: expected <double> in brackets [line %d]%s\n",
+                   RED,
+                   line,
+                   RESET);
+            break;
+        } else if (((i > minus) || (i < len - 1)) && (str[i] == '.')) {
+            floating_point++;
+            if (floating_point > 1) {
+                error_flag_container = 4;
+                printf("%sError: expected <double> in brackets [line "
+                       "%d]%s\n",
+                       RED,
+                       line,
+                       RESET);
+                break;
+            }
+        } else if (
+                ((i > minus) || (i < len - 1))
+                && ((isdigit(str[i]) == 0) && (str[i] != '.'))) {
+            error_flag_container = 4;
+            printf("%sError: expected <double> in brackets [line %d]%s\n",
+                   RED,
+                   line,
+                   RESET);
+            break;
+        }
+    }
+    return error_flag_container;
+}
+
+// need strtok in extractor functions + in circle must be 2 brackets, other 4
+// brackets strtok must have 3 values in circle
+//                  6 values in triangle
+//                  even (2*n) values in polygon
